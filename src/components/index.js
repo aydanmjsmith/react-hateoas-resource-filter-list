9dfa83c3b5b600b1feff669da2ec24be2cc51b4f
Aydan Smith <aydanmjsmith@gmail.com>
2022-05-12 12:35:30 +0100

    update exported files.

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { default as EntityFilterList } from './ui/EntityFilterList';
 export { default as EntityLookupModal } from './ui/EntityLookupModal';
+export { default as EntityTableTitle } from './ui/EntityTableTitle';
+export { default as EntityPageControls } from './ui/EntityPageControls';
 export { default as Test } from './ui/Test';
